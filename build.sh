6b7fd39b834ca1706b06ac631ad3591662aaacc7
Joe Maples <joe@frap129.org>
2017-11-13 17:11:23 -0500

    build+Makefile: Add versioning system
    Signed-off-by: Joe Maples <joe@frap129.org>

--- build.sh
+++ build.sh
@@ -4,21 +4,21 @@
 
 export ARCH=arm64
 export SUBARCH=arm64
-export CLANG_PATH=~/android/kernel/dtc4/bin
 export CLANG_TRIPLE=aarch64-linux-gnu-
+export CLANG_PATH=~/android/kernel/dtc4/bin
 export CROSS_COMPILE=~/android/kernel/aarch64-linaro-linux-gnu/bin/aarch64-linux-gnu-
+export REL="v1"
+export TESTVER="-$1"
 
 make mrproper -j8
 make electron_defconfig
 make CC="ccache $CLANG_PATH/clang" -j8
 
-if [[ -n "$1" ]]; then
-    rm ../img/device/google/wahoo-kernel/Image.lz4-dtb *.ko;
-    cp arch/arm64/boot/Image.lz4-dtb ../img/device/google/wahoo-kernel/Image.lz4-dtb;
-    find . -name *.ko | xargs -I{} cp {} ../img/device/google/wahoo-kernel/;
-    cd ../img;
-    . build/envsetup.sh;
-    lunch aosp_taimen-user;
-    make clean;
-    make bootimage -j8;
-fi;
+rm ../img/device/google/wahoo-kernel/Image.lz4-dtb ../img/device/google/wahoo-kernel/*.ko;
+cp arch/arm64/boot/Image.lz4-dtb ../img/device/google/wahoo-kernel/Image.lz4-dtb;
+find . -name *.ko | xargs -I{} cp {} ../img/device/google/wahoo-kernel/;
+cd ../img;
+. build/envsetup.sh;
+lunch aosp_taimen-user;
+make installclean;
+make bootimage -j8;
