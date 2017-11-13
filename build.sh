7457cb6e697628d3153353f3f297f1fc0820edd8
Joe Maples <joe@frap129.org>
2017-11-13 01:05:11 -0500

    build: Use DragonTC, don't modify PATH
    Signed-off-by: Joe Maples <joe@frap129.org>

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@
 
 export ARCH=arm64
 export SUBARCH=arm64
-export CLANG_PATH=~/android/rom/oreo/prebuilts/clang/host/linux-x86/clang-3957855/bin/
+export CLANG_PATH=~/android/kernel/dtc4/bin
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=~/android/kernel/aarch64-linaro-linux-gnu/bin/aarch64-linux-gnu-
 
 make mrproper -j8
 make electron_defconfig
-make PATH=${CLANG_PATH}:${PATH} CC="ccache clang" -j8
+make CC="ccache $CLANG_PATH/clang" -j8
 
 if [[ -n "$1" ]]; then
     rm ../img/device/google/wahoo-kernel/Image.lz4-dtb *.ko;
